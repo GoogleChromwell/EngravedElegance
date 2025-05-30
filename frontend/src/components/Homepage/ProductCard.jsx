bd72c553115796165b6b07e0be6c0dd4d0c4af79
Cromwell Naval <navalcromwell@gmail.com>
2025-05-30 23:35:50 +0800

    created display products

--- frontend/src/components/Homepage/ProductCard.jsx
+++ frontend/src/components/Homepage/ProductCard.jsx
@@ -1,29 +1,32 @@
 import React from "react";
 
-export default function ProductCard() {
+export default function ProductCard({ name, description, price, quantity }) {
   return (
     <div
       className="
-    w-fit h-fit p-[10px] bg-white border border-primary-dark border-opacity-30 flex flex-col
-    font-poppins gap-2"
+    w-52  p-[10px] bg-white border border-primary-dark border-opacity-30 flex flex-col
+    font-poppins justify-between"
     >
-      <div className="border border-primary-dark border-opacity-20 w-full h-36 "></div>
-
-      <h2 className="text-[14px] font-medium">Wooden Coaster</h2>
-      <p className="text-[12px] w-44">
-        Circular wooden engraving available in plain or colored design.
-      </p>
-
-      <div className="flex items-center justify-between">
-        <h1 className="text-[16px] font-semibold">₱50.00</h1>
-        <div className="border border-primary-dark border-opacity-15 rounded-custom-xs p-[4px]">
-            <p className="text-[10px] font-medium">100 Items</p>
+      <div className="flex flex-col gap-2">
+        <div className="border border-primary-dark border-opacity-20 w-full h-36 "></div>
+        <div>
+          <h2 className="text-[14px] font-medium">{name}</h2>
+          <p className="text-[10px] w-44">{description}</p>
         </div>
       </div>
 
-      <button className="bg-primary-dark text-[12px] text-white font-medium p-[6px] rounded-custom-xs">
-        Add to Cart
-      </button>
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-[16px] font-semibold">₱{price}</h1>
+          <div className="border border-primary-dark border-opacity-15 rounded-custom-xs p-[4px]">
+            <p className="text-[10px] font-medium">{quantity} items</p>
+          </div>
+        </div>
+
+        <button className="bg-primary-dark text-[12px] text-white font-medium p-[6px] rounded-custom-xs">
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 }
--- frontend/src/components/Homepage/Products.jsx
+++ frontend/src/components/Homepage/Products.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import axios from "axios";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost/Engraved-Clone/EngravedElegance/backend/DisplayProducts.php"
+      )
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching products: ", error);
+      });
+  }, []);
   return (
     <div className="p-[10px] flex flex-col gap-4 justify-center items-center">
       <div className="flex gap-4">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
-      <div className="flex gap-4">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((product, index) => (
+          <ProductCard 
+          key={{index}}
+          name={product["product-name"]}
+          description={product["product-description"]}
+          price={product.price}
+          quantity={product.quantity}/>
+        ))}
       </div>
     </div>
   );
