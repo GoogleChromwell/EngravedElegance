a1426b813be2fde8a24726f4ce65e4ed8d240f9e
Cromwell Naval <navalcromwell@gmail.com>
2025-06-04 22:22:25 +0800

    added new products

--- frontend/src/components/Homepage/Products.jsx
+++ frontend/src/components/Homepage/Products.jsx
@@ -18,7 +18,7 @@ export default function Products() {
   }, []);
   return (
     <div className="p-[10px] flex flex-col gap-4 justify-center items-center">
-      <div className="flex gap-4">
+      <div className="grid grid-cols-4 gap-6">
         {products.map((product) => (
           <ProductCard
             key={product.product_id}
